3d2ce5f7461fcf0c80d8ecfbcb83726319c923b9
treephesians <ephesi4ns623@gmail.com>
2023-08-01 23:43:31 +0900

    add webview

---
+++ web_view/lib/screen/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
+class HomeScreen extends StatelessWidget {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Code Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              controller.loadRequest(homeUrl);
+            },
+            icon: const Icon(Icons.home),
+          ),
+        ],
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+      // WebView(
+      //   onWebViewCreated: (WebViewController controller) {
+      //     this.controller = controller;
+      //   },
+      //   initialUrl: homeUrl,
+      //   javascriptMode: JavascriptMode.unrestricted,
+      // ),
+    );
+  }
+}
