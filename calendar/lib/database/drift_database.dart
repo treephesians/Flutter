950db67a44db2547af2a80cab78831e15afceca5
ephesi4ns <ephesi4ns623@gmail.com>
2023-08-07 23:52:26 +0900

    add Drift

---
+++ calendar/lib/database/drift_database.dart
@@ -0,0 +1,41 @@
+// private 값들은 불러올 수 있다.
+import 'dart:io';
+
+import 'package:calendar/model/category_color.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import '../model/schedule.dart';
+import 'package:path/path.dart' as p;
+// private 값까지 불러올 수 있다.
+part 'drift_database.g.dart';
+
+@DriftDatabase(
+  tables: [
+    Schedules,
+    CategoryColors,
+  ],
+)
+class LocalDatabase extends _$LocalDatabase {
+  LocalDatabase() : super(_openConnection());
+
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+
+  Future<List<CategoryColor>> getCategoryCOlors() =>
+      select(categoryColors).get();
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
---
+++ calendar/lib/model/category_color.dart
@@ -0,0 +1,6 @@
+import 'package:drift/drift.dart';
+
+class CategoryColors extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get hexCode => text()();
+}
